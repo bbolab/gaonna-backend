b41b451be5d16eddf58b569800b9395da210c8ca
Dongkyun Yoo <me@sokdak.me>
2021-05-01 17:53:07 +0900

    chore: generate report configuration for CI/CD (#7)

---
+++ core/src/test/java/com/bbolab/gaonna/core/TestingTest.java
@@ -0,0 +1,13 @@
+package com.bbolab.gaonna.core;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TestingTest {
+    @Test
+    public void testTest() {
+
+    }
+}
