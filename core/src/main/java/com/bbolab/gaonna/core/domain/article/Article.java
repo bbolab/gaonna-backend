55ee685491ff3a671008d6e68452dbd70b1e8763
blupine <skwint11@gmail.com>
2021-05-10 18:43:17 +0900

    feat: add test for likeCount

--- core/src/main/java/com/bbolab/gaonna/core/domain/article/Article.java
+++ core/src/main/java/com/bbolab/gaonna/core/domain/article/Article.java
@@ -73,6 +73,7 @@ public class Article {
         if (comments.contains(memberArticleComment)) {
             return false;
         }
+        // TODO : add commentCount increment
         memberArticleComment.setArticle(this);
         return comments.add(memberArticleComment);
     }
@@ -81,7 +82,15 @@ public class Article {
         if (articleLikeMembers.contains(memberArticleLike)) {
             return false;
         }
+        this.likeCount++;
         memberArticleLike.setArticle(this);
         return articleLikeMembers.add(memberArticleLike);
+    }
+
+    public void removeMemberArticleLike(MemberArticleLike memberArticleLike) {
+        if (this.articleLikeMembers.contains(memberArticleLike)) {
+            this.likeCount--;
+            this.articleLikeMembers.remove(memberArticleLike);
+        }
     }
 }
