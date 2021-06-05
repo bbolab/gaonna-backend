243c7819857fa8882695af9e3793b7f43f6ebb95
blupine <skwint11@gmail.com>
2021-06-05 14:47:57 +0900

    chore: remove unusing library import

--- api/src/main/java/com/bbolab/gaonna/api/v1/controller/CommentController.java
+++ api/src/main/java/com/bbolab/gaonna/api/v1/controller/CommentController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.UUID;
 
--- api/src/test/java/com/bbolab/gaonna/api/controller/MockCommentControllerTest.java
+++ api/src/test/java/com/bbolab/gaonna/api/controller/MockCommentControllerTest.java
@@ -19,7 +19,10 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
