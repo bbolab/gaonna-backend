dc42714d43bb619195567e9615539d1d58a46bf5
blupine <skwint11@gmail.com>
2021-07-27 19:25:43 +0900

    feat: add repository for region

--- core/src/main/java/com/bbolab/gaonna/core/repository/RegionL1Repository.java
+++ core/src/main/java/com/bbolab/gaonna/core/repository/RegionL1Repository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 @Transactional(readOnly = true)
 public interface RegionL1Repository extends JpaRepository<RegionL1, UUID> {
+    Optional<RegionL1> findByAdmCode(String admCode);
 }
--- core/src/main/java/com/bbolab/gaonna/core/repository/RegionL3Repository.java
+++ core/src/main/java/com/bbolab/gaonna/core/repository/RegionL3Repository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 @Transactional(readOnly = true)
 public interface RegionL3Repository extends JpaRepository<RegionL3, UUID> {
+    Optional<RegionL3> findByAdmCode(String admCode);
 }
