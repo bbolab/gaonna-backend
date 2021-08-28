c5bc71d22aaf1a95f55268ef1e67da40e64c4984
Dongkyun Yoo <me@sokdak.me>
2021-08-28 17:54:11 +0900

    feat: implement file service and entity (#5)
    * feat: implement file service and entity
    
    * feat: get fileserviceroot from config

--- core/src/main/java/com/bbolab/gaonna/core/repository/FileRepository.java
+++ core/src/main/java/com/bbolab/gaonna/core/repository/FileRepository.java
@@ -1,12 +1,12 @@
 package com.bbolab.gaonna.core.repository;
 
-import com.bbolab.gaonna.core.domain.SampleEntity;
+import com.bbolab.gaonna.core.domain.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface SampleRepository extends JpaRepository<SampleEntity, UUID> {
+public interface FileRepository extends JpaRepository<File, UUID> {
 
 }
