fb5025cef594d67a7e3d928cacc93a545e9f643b
blupine <skwint11@gmail.com>
2022-04-10 21:21:16 +0900

    feat: 불필요 코드 삭제 [GAON-13]

--- api/src/main/java/com/bbolab/gaonna/api/security/model/provider/NaverOAuth2UserInfo.java
+++ api/src/main/java/com/bbolab/gaonna/api/security/model/provider/NaverOAuth2UserInfo.java
@@ -3,32 +3,29 @@ package com.bbolab.gaonna.api.security.model.provider;
 import java.util.Map;
 
 public class NaverOAuth2UserInfo extends OAuth2UserInfo {
-
-    private Map<String, Object> userAttributes;
-
+    
     public NaverOAuth2UserInfo(Map<String, Object> attributes) {
-        super(attributes);
-        userAttributes = (Map<String, Object>)attributes.get("response");
+        super((Map<String, Object>) attributes.get("response"));
     }
 
     @Override
     public String getId() {
-        return (String) userAttributes.get("id");
+        return (String) attributes.get("id");
     }
 
     @Override
     public String getName() {
-        return (String) userAttributes.get("name");
+        return (String) attributes.get("name");
     }
 
     @Override
     public String getEmail() {
-        return (String) userAttributes.get("email");
+        return (String) attributes.get("email");
     }
 
     @Override
     public String getImageUrl() {
-        return (String) userAttributes.get("profile_image");
+        return (String) attributes.get("profile_image");
     }
 
 }
